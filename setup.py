c54a21bf714cf7f69fb19a9d6521e4076158be7a
Oleksii Palamarchuk <oleksiypalamarchuck@gmail.com>
2025-01-18 15:12:16 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License"
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
